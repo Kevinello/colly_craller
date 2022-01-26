42e99320738c4c2c539e61e887d64d1dd4a87c1a
Kevinello <1051360477@qq.com>
2022-01-26 16:59:59 +0800

    update

---
+++ anticrawl/user_agent.go
@@ -0,0 +1,14 @@
+package anticrawl
+
+import "math/rand"
+
+// 生成范围
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString() string {
+	b := make([]byte, rand.Intn(10)+10)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
