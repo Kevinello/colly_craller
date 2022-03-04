4c7a23b203f5ec5f61f7b48974ce625bb14c0e49
Kevinello <1051360477@qq.com>
2022-03-04 22:24:48 +0800

    init storage and pg

--- internal/jd/storage/storage.go
+++ internal/jd/storage/storage.go
@@ -2,14 +2,27 @@ package storage
 
 import (
 	"encoding/json"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"kevinello.ltd/kevinello/collycrawler/internal/pkg/log"
 )
 
-var ()
+var (
+	DB *gorm.DB
+)
 
 func init() {
 	// TODO: 初始化数据库
+	dsn := "host=localhost user=kevinello password=jdCrawler2022 dbname=jd_data port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.GLogger.Alert("can't connect to database")
+		os.Exit(1)
+	}
 }
 
 func (item *Item) save() (err error) {
